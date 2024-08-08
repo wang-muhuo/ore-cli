9647cb9644030b5a24b2a812f93fe371d031c61f
wang-muhuo <36071953+wang-muhuo@users.noreply.github.com>
2024-08-08 11:34:28 +0800

    Update mine.rs

--- src/mine.rs
+++ src/mine.rs
@@ -175,7 +175,7 @@ impl Miner {
 	// 传入最大困难值
          rerutn  (Solution::new(best_hash.d, best_nonce.to_le_bytes()),best_difficulty);
 	}
-
+	}
     }
 
     pub fn check_num_cores(&self, threads: u64) {
